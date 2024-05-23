c89271ea9bf0efd602814e4730765343dd7e4bc3
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-23 02:31:47 +0000

    update

--- memorytest/memorytest.go
+++ memorytest/memorytest.go
@@ -5,11 +5,11 @@ import (
 	"os/exec"
 )
 
-// sysbench --test=memory --num-threads=1 --memory-block-size=1M --memory-total-size=102400G --memory-oper=read --max-time=5 --memory-access-mode=seq run 2>&1
-
-// sysbench --test=memory --num-threads=1 --memory-block-size=1M --memory-total-size=102400G --memory-oper=write --max-time=5 --memory-access-mode=seq run 2>&1
-
 func runSysBenchCommand(numThreads, oper, maxTime string) (string, error) {
+	// 读测试
+	// sysbench --test=memory --num-threads=1 --memory-block-size=1M --memory-total-size=102400G --memory-oper=read --max-time=5 --memory-access-mode=seq run 2>&1
+	// 写测试
+	// sysbench --test=memory --num-threads=1 --memory-block-size=1M --memory-total-size=102400G --memory-oper=write --max-time=5 --memory-access-mode=seq run 2>&1
 	command := exec.Command("sysbench", "--test=memory", "--num-threads="+numThreads, "--memory-block-size=1M", "--memory-total-size=102400G", "--memory-oper="+oper, "--max-time="+maxTime, "--memory-access-mode=seq", "run", "2>&1")
 	output, err := command.CombinedOutput()
 	return string(output), err
